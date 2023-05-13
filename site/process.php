fd9c26f031f5a806e29a2835514549ff978a00a3
Wallace Pontes <wallacepnts@gmail.com>
2023-05-13 20:23:54 -0300

    22/05/2013

---
+++ site/process.php
@@ -0,0 +1,78 @@
+<?php
+
+//Retrieve form data. 
+//GET - user submitted data using AJAX
+//POST - in case user does not support javascript, we'll use POST instead
+$name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
+$email = ($_GET['email']) ?$_GET['email'] : $_POST['email'];
+$comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
+
+//flag to indicate which method it uses. If POST set it to 1
+if ($_POST) $post=1;
+
+//Simple server side validation for POST data, of course, you should validate the email
+if (!$name) $errors[count($errors)] = 'Por favor digite seu nome.';
+if (!$email) $errors[count($errors)] = 'Por favor digite seu e-mail.'; 
+if (!$comment) $errors[count($errors)] = 'Por favor digite seu comentário.'; 
+
+//if the errors array is empty, send the mail
+if (!$errors) {
+
+	//recipient
+	$to = 'contatoidofilmes@gmail.com';	
+	//sender
+	$from = $name . ' <' . $email . '>';
+	
+	//subject and the html message
+	$subject = 'Comentário de ' . $name;	
+	$message = '
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<head></head>
+	<body>
+	<table>
+		<tr><td>Nome:</td><td>' . $name . '</td></tr>
+		<tr><td>Email:</td><td>' . $email . '</td></tr>
+		<tr><td>Comentário:</td><td>' . nl2br($comment) . '</td></tr>
+	</table>
+	</body>
+	</html>';
+
+	//send the mail
+	$result = sendmail($to, $subject, $message, $from);
+	
+	//if POST was used, display the message straight away
+	if ($_POST) {
+		if ($result) echo 'Obrigado! Recebi sua mensagem.';
+		else echo 'Por favor, tente novamente mais tarde';
+		
+	//else if GET was used, return the boolean value so that 
+	//ajax script can react accordingly
+	//1 means success, 0 means failed
+	} else {
+		echo $result;	
+	}
+
+//if the errors array has values
+} else {
+	//display the errors message
+	for ($i=0; $i<count($errors); $i++) echo $errors[$i] . '<br/>';
+	echo '<a href="form.php">Back</a>';
+	exit;
+}
+
+
+//Simple mail function with HTML header
+function sendmail($to, $subject, $message, $from) {
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+	$headers .= 'From: ' . $from . "\r\n";
+	
+	$result = mail($to,$subject,$message,$headers);
+	
+	if ($result) return 1;
+	else return 0;
+}
+
+?>
